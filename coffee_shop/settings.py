e244afe7ae88322ac40266b2d0ed3d59a3be14b5
rockband_666 <rockband_666@hotmail.com>
2024-07-14 23:24:25 -0400

    agregado en el django admin las fotos y todo las usuabilidades que se tiene en el admin

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -117,9 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = "static"
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Additional locations of static files
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
