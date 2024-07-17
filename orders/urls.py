19bcaa70f5c7dcf09051fd323686ab90be4bbd2a
rockband_666 <rockband_666@hotmail.com>
2024-07-17 00:29:50 -0400

    se agrego la orden pero para cada usuario  pero tiene falllas no hace el pedido que uno quiere y envia un error de 405

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,8 @@
-from .views import MyOrderView
+from .views import MyOrderView, CreateOrderProductView
 from django.urls import path
 
 
 urlpatterns = [
-    path ('mi-orden', MyOrderView.as_view(), name="my_order")
+    path ('mi-orden', MyOrderView.as_view(), name="my_order"),
+    path ('agregar-producto', CreateOrderProductView.as_view(), name="add_product")
 ]
